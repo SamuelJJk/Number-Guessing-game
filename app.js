859a81ee3d85045b4fb0a610e0dad22fb517152d
SamuelJJk <samueljean.j24@gmail.com>
2024-03-21 14:56:44 -0400

    added a gifs that shuffle depending on score and short some code

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
-
 let guess = 0;
 let answer = 0;
+let tries = 10;
 let myPicks = document.querySelector('.myPicks')
+let counter = document.querySelector('.counter')
 
 //Random number generator
 const randomNum = () =>{
@@ -12,31 +13,39 @@ const randomNum = () =>{
 answer = randomNum()
 console.log(answer)
 
-let tries = 10;
-let counter = document.querySelector('.counter')
 // counter.textContent = tries
 counter.innerHTML = tries
 
 //prompt for users Guesses
 const userGuess = () => {
-    console.log(`This was the num before ${guess}`)
     let pickNum = prompt('guess a number between 1 - 100')
     guess = pickNum
-    console.log(`This is the Current num ${guess}`)
-    myPicks.append(pickNum)
+    while (myPicks.firstChild) {
+        myPicks.removeChild(myPicks.firstChild); 
+    }
+}
+const scoreCheck = () => {
+    let image = document.querySelector('.image')
+    switch(true) {
+        case(tries >= 6 && tries < 10):
+            image.setAttribute('src','https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGI1YzAxOWdhNGRobDFpeW1yZWFxeWs4MWR1aTZhdjRtazAzbGE1ZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o75269cVD7heeSOgE/giphy.gif');
+            break;
+        case( tries < 6 && tries > 2):
+            image.setAttribute('src', 'https://i.gifer.com/SBrf.gif')
+            break;
+        case (tries <= 2):
+            image.setAttribute('src', 'https://media1.tenor.com/m/mWFelILO6L8AAAAC/spongebob-squarepants-spongebob.gif')
+            default:
+                // image.setAttribute('src','https://66.media.tumblr.com/ce3a0f588060677739d4c1dc3c9f04a3/tumblr_nvqr429lvX1tq4of6o1_500.gif')
+
+    }
+}
+// start for the game
+const game = () => {
+    userGuess()
+    guess == answer ? alert('You have won the game'):
+    guess > answer? (tries--, counter.innerHTML = tries, myPicks.append(`You're guess was too high ${guess}`),scoreCheck()):
+    guess < answer? (tries--, counter.innerHTML = tries, myPicks.append(`You're guess was too low ${guess}`),scoreCheck()):
+    'please enter a number';
 }
 
-const game = () => {
-    while (tries > 0){
-    userGuess();
-    if (guess == answer){
-        alert('you have won')
-        return
-    }
-    else{
-        tries --
-        counter.innerHTML = tries
-        return
-    }
-}
-}
